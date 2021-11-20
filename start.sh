537b9e8bd559054f4f0abaafe1fd22ab2e1f0bef
ashissah <ashissah@in.ibm.com>
2021-11-20 20:13:33 +0530

    Test again
    Signed-off-by: ashissah <ashissah@in.ibm.com>

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -e
 set -eux;
-pid=$!
-./var/dbb_home/wlp/bin/server start dbb &
 
+./var/dbb_home/wlp/bin/server start dbb &
+pid=$!
 wait $pid
-tail -f /var/dbb_home/wlp/usr/servers/dbb/logs/dbb.log
+tail -f /var/dbb_home/wlp/usr/servers/dbb/logs/dbb.log &
+pid=$!
+wait $pid
